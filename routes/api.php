ec94f2f21a9093e8f8da75c233cc4ff46a30a375
MikeeOo <michal.olszewskki@gmail.com>
2024-09-10 11:42:01 +0200

    added API middleware for readability

--- routes/api.php
+++ routes/api.php
@@ -4,9 +4,11 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 // These routes are loaded by the RouteServiceProvider and all of them will be assigned to the "api" middleware group.
-Route::get('/', fn () => 'API WORKS!');
+Route::middleware('api')->group(function () {
+    Route::get('/', fn () => 'API WORKS!');
 
-Route::prefix('products')->group(function () {
-    Route::get('/', [ProductController::class, 'index'])->name('products.index');
-    Route::get('/{product}', [ProductController::class, 'show'])->name('products.show');
+    Route::prefix('products')->group(function () {
+        Route::get('/', [ProductController::class, 'index'])->name('products.index');
+        Route::get('/{product}', [ProductController::class, 'show'])->name('products.show');
+    });
 });
