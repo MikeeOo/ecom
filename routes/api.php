73346b7144478c49d9792f4fdb0b84161dd7775f
MikeeOo <michal.olszewskki@gmail.com>
2024-09-10 11:36:46 +0200

    added route names to API router

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', fn () => 'API WORKS!');
 
 Route::prefix('products')->group(function () {
-    Route::get('/', [ProductController::class, 'index']);
-    Route::get('/{product}', [ProductController::class, 'show']);
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    Route::get('/{product}', [ProductController::class, 'show'])->name('products.show');
 });
