72e1cc2ed0898107a30695ee8deae35ef0220e6d
MikeeOo <michal.olszewskki@gmail.com>
2024-12-05 09:48:56 +0100

    created Router components

---
+++ frontend/src/Router.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import Default from "./layouts/Default.tsx";
+import Home from "./pages/Home.tsx";
+import Admin from "./layouts/Admin.tsx";
+import Error from "./layouts/Error.tsx";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<App />}>
+                    <Route element={<Default />}>
+                        <Route index element={<Home />} />
+                        <Route path="/home" element={<Home />} />
+                    </Route>
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="*" element={<Error />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
