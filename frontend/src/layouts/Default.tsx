de1184275b0c130f81ac00f8cf46b807746d52f9
MikeeOo <michal.olszewskki@gmail.com>
2024-12-18 12:02:05 +0100

    tested Text component

--- frontend/src/layouts/Default.tsx
+++ frontend/src/layouts/Default.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router";
 import NavBar from "../partials/NavBar.tsx";
 import Footer from "../partials/Footer.tsx";
+import { Text } from "../common/atoms";
 
 export default function Default() {
     return (
         <>
             <NavBar />
-            <div>That's default layout</div>
+            <div>
+                <Text.H2 text="That's default layout" align="center" />
+            </div>
             <Outlet />
             <Footer />
         </>
