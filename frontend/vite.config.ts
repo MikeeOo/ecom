ee638d681b8347ebf4b80bf30ab89cbc33e0e982
MikeeOo <michal.olszewskki@gmail.com>
2024-12-21 10:46:14 +0100

    initialized postcss-import

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+// IT HAS TO BE LIKE THAT
+import postcssImport from "postcss-import";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    css: {
+        postcss: {
+            plugins: [postcssImport(), tailwindcss, autoprefixer],
+        },
+    },
+
     server: {
         port: 3000,
     },
