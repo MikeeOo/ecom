f4a4315fa53b14d2ed7f3a5fed35c332fb20badd
MikeeOo <michal.olszewskki@gmail.com>
2024-12-21 10:40:35 +0100

    Created Container component

--- frontend/src/common/containers/index.ts
+++ frontend/src/common/containers/index.ts
@@ -1,3 +1,4 @@
+export { Container } from "./Container/Container.tsx";
 export { Flex } from "./Flex/Flex.tsx";
 export { Form } from "./Form/Form.tsx";
 export { Wrapper } from "./Wrapper/Wrapper.tsx";
