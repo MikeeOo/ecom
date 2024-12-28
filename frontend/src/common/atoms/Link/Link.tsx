5e6e6e4c4c33c1e3ad720228dd171fd45707a3f5
MikeeOo <michal.olszewskki@gmail.com>
2024-12-28 12:20:28 +0100

    created Link component

---
+++ frontend/src/common/atoms/Link/Link.tsx
@@ -0,0 +1,56 @@
+import { JSX, FC, useMemo } from "react";
+import { twMerge } from "tailwind-merge";
+import { Link as RouterLink, NavLink } from "react-router";
+import { shapeClasses } from "../types.ts";
+import { components, Components, Tag, Props, typeClasses } from "./Link.types";
+const createComponent = (tag: Tag): FC<Props> => {
+    const Component: FC<Props> = ({
+        children,
+        route,
+        type,
+        shape,
+        icon,
+        iconLeft,
+        text,
+        iconRight,
+        content,
+        className,
+    }) => {
+        const Tag: Tag = tag;
+        const styles: string = useMemo(
+            () => twMerge(typeClasses[type], shape && shapeClasses[shape], className),
+            [type, shape, className],
+        );
+        const linkContent: JSX.Element = (
+            <>
+                {icon || iconLeft}
+                {text && text}
+                {iconRight && iconRight}
+                {content && content}
+                {children && children}
+            </>
+        );
+        if (tag === "a")
+            return (
+                <a href={route} className={styles}>
+                    {linkContent}
+                </a>
+            );
+        if (tag === NavLink)
+            return (
+                <NavLink to={route} className={({ isActive }) => twMerge(styles, isActive && "utils-active")}>
+                    {linkContent}
+                </NavLink>
+            );
+        return (
+            <Tag to={route} className={styles}>
+                {linkContent}
+            </Tag>
+        );
+    };
+    Component.displayName = `Link.${tag === "a" ? "A" : tag === RouterLink ? "Router" : "Nav"}`;
+    return Component;
+};
+export const Link = Object.fromEntries(
+    Object.entries(components).map(([key, tag]) => [key, createComponent(tag as Tag)]),
+) as Components;
---
+++ frontend/src/common/atoms/Link/Link.types.ts
@@ -0,0 +1,34 @@
+import { FC, ReactNode } from "react";
+import { shapeClasses } from "../types.ts";
+import { Link as RouterLink, NavLink } from "react-router";
+export const components = {
+    Router: RouterLink,
+    Nav: NavLink,
+    A: "a",
+} as const;
+export type Tag = (typeof components)[keyof typeof components];
+// export const baseClasses = {
+//     Router: "base-router-link",
+//     Nav: "base-nav-link",
+//     A: "base-a",
+// } as const;
+export const typeClasses = {
+    text: "link-text",
+    "btn-primary": "btn-primary",
+    wrapper: "link-wrapper",
+} as const;
+export type Props = {
+    children?: ReactNode;
+    route: string;
+    type: keyof typeof typeClasses;
+    shape?: keyof typeof shapeClasses;
+    icon?: ReactNode;
+    iconLeft?: ReactNode;
+    text?: string;
+    iconRight?: ReactNode;
+    content?: ReactNode;
+    className?: string;
+};
+export type Components = {
+    [K in keyof typeof components]: FC<Props>;
+};
