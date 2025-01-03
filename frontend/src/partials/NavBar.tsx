377ef2993f9bbf2a556f8e7a109709500477251a
MikeeOo <michal.olszewskki@gmail.com>
2025-01-03 12:41:32 +0100

    Navbar scaffold

--- frontend/src/partials/NavBar.tsx
+++ frontend/src/partials/NavBar.tsx
@@ -1,7 +1,34 @@
-export default function NavBar() {
+import { ReactElement } from "react";
+import { Flex, Wrapper } from "../common/containers";
+import { Link } from "../common/atoms";
+import { LuShoppingCart, LuUser } from "react-icons/lu";
+import { MdOutlineStoreMallDirectory } from "react-icons/md";
+export default function NavBar(): ReactElement {
     return (
-        <>
-            <div className="bg-black text-white">That's Navbar</div>
-        </>
+        <Wrapper.Header className="fixed w-full border-b border-black bg-white">
+            <Flex.Nav x="between" y="center" container>
+                <Link.Nav
+                    route="/"
+                    type="nav"
+                    shape="square"
+                    iconLeft={<MdOutlineStoreMallDirectory className="text-2xl" />}
+                    text="Ecom"
+                    className="flex items-center gap-1 border-black"
+                />
+                <Flex.Ul>
+                    <Wrapper.Li>
+                        <Link.Nav route="/login" type="nav" shape="rectangle" icon={<LuUser className="text-2xl" />} />
+                    </Wrapper.Li>
+                    <Wrapper.Li>
+                        <Link.Nav
+                            route="/cart"
+                            type="nav"
+                            shape="rectangle"
+                            icon={<LuShoppingCart className="text-2xl" />}
+                        />
+                    </Wrapper.Li>
+                </Flex.Ul>
+            </Flex.Nav>
+        </Wrapper.Header>
     );
 }
