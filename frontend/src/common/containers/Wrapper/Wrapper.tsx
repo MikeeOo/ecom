983e42b0a355358adb7c43ec9aa234b06064e85c
MikeeOo <michal.olszewskki@gmail.com>
2025-01-03 12:47:12 +0100

    added container class to Wrapper.tsx

--- frontend/src/common/containers/Wrapper/Wrapper.tsx
+++ frontend/src/common/containers/Wrapper/Wrapper.tsx
@@ -2,10 +2,12 @@ import { FC } from "react";
 import { capitalize } from "../../../utils/string-utils.ts";
 import { components, Tag } from "../types.ts";
 import { Props, Components } from "./Wrapper.types.ts";
+import { twMerge } from "tailwind-merge";
 const createComponent = (tag: Tag): FC<Props> => {
-    const Component: FC<Props> = ({ children, content, className }) => {
+    const Component: FC<Props> = ({ children, content, className, container }) => {
         const Tag: Tag = tag;
-        return <Tag className={className}>{children || content}</Tag>;
+        const styles: string = twMerge(container && "container", className);
+        return <Tag className={styles}>{children || content}</Tag>;
     };
     Component.displayName = `Wrapper.${capitalize(tag)}`;
     return Component;
--- frontend/src/common/containers/Wrapper/Wrapper.types.ts
+++ frontend/src/common/containers/Wrapper/Wrapper.types.ts
@@ -4,6 +4,7 @@ export type Props = {
     children?: ReactNode;
     content?: ReactNode;
     className?: string;
+    container?: boolean;
 };
 export type Components = {
     [K in keyof typeof components]: FC<Props>;
