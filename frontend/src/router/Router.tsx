6083eb1299e3173de0a043e605b5389affcb4a03
MikeeOo <michal.olszewskki@gmail.com>
2025-01-03 12:52:38 +0100

    fixed router directories

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,25 +1,25 @@
-import { JSX } from "react";
+import { ReactElement } from "react";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "../App.tsx";
-import Default from "../layouts/Default.tsx";
-import Home from "../pages/Home.tsx";
-import Login from "../pages/Login.tsx";
-import Register from "../pages/Register.tsx";
-import Admin from "../layouts/Admin.tsx";
-import Error from "../layouts/Error.tsx";
-export default function Router(): JSX.Element {
+import DefaultLayout from "../layouts/DefaultLayout.tsx";
+import HomePage from "../pages/HomePage.tsx";
+import LoginPage from "../pages/LoginPage.tsx";
+import RegisterPage from "../pages/RegisterPage.tsx";
+import AdminLayout from "../layouts/AdminLayout.tsx";
+import ErrorLayout from "../layouts/ErrorLayout.tsx";
+export default function Router(): ReactElement {
     return (
         <BrowserRouter>
             <Routes>
                 <Route element={<App />}>
-                    <Route element={<Default />}>
-                        <Route index element={<Home />} />
-                        <Route path="/home" element={<Home />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
+                    <Route element={<DefaultLayout />}>
+                        <Route index element={<HomePage />} />
+                        <Route path="/home" element={<HomePage />} />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
                     </Route>
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="*" element={<Error />} />
+                    <Route path="/admin" element={<AdminLayout />} />
+                    <Route path="*" element={<ErrorLayout />} />
                 </Route>
             </Routes>
         </BrowserRouter>
