680384e34c55830cd53b56b6c7ca199314693bc3
MikeeOo <michal.olszewskki@gmail.com>
2025-01-04 11:30:22 +0100

    improved styling and added redirection Link to LoginPage.tsx

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,8 +1,9 @@
 import { ReactElement } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Input, Submit, Text } from "../common/atoms";
-import { Flex, Form } from "../common/containers";
+import { ROUTES } from "../constants";
+import { Input, Link, Submit, Text } from "../common/atoms";
+import { Flex, Wrapper, Form } from "../common/containers";
 import { LoginInput, loginSchema } from "../schemas/login.schema.ts";
 export default function LoginPage(): ReactElement {
     const { register, handleSubmit } = useForm<LoginInput>({
@@ -10,19 +11,19 @@ export default function LoginPage(): ReactElement {
     });
     const onSubmit: SubmitHandler<LoginInput> = async (data: LoginInput): Promise<void> => console.log(data);
     return (
-        <Flex.Section x="center" y="center" className="min-h-[calc(100vh_-_82px)] px-3">
-            <Flex.Div dir="col" y="center" className="border-default relative p-9">
-                <label htmlFor="djfkgklsdf" />
+        <Flex.Section y="center" dir="col" gap="6" className="min-h-min-h-[calc(100vh_-_82px)] px-3 pt-12 sm:pt-[72px]">
+            <Wrapper.Div className="border-default relative w-full max-w-[384px] px-3 pb-6 pt-6 sm:px-6">
                 <Text.H1
                     size="md"
                     text="Sign In"
-                    className="border-default absolute top-0 -translate-y-1/2 bg-white px-3 py-1"
+                    className="border-default absolute left-1/2 top-0 -translate-x-1/2 -translate-y-1/2 bg-white px-3 py-1"
                 />
-                <Form id="login-form" onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+                <Form id="login-form" onSubmit={handleSubmit(onSubmit)}>
                     <Input.Email
                         id="user-email"
                         name="email"
                         labelText="Email"
+                        // error="Wymagany adres e-mail"
                         placeholder="you@example.com"
                         register={register}
                     />
@@ -30,13 +31,18 @@ export default function LoginPage(): ReactElement {
                         id="user-password"
                         name="password"
                         labelText="Password"
-                        error="dkhj"
                         placeholder="password"
                         register={register}
                     />
-                    <Submit.Primary shape="rectangle" text="Sign In" />
+                    <Submit.Primary shape="rectangle" text="Sign In" className="w-full" />
                 </Form>
-            </Flex.Div>
+            </Wrapper.Div>
+            <Link.Router
+                route={ROUTES.REGISTER}
+                type="text"
+                text="Don't have an account? Sign up."
+                className="text-sm leading-none text-zinc-700 hover:text-black"
+            />
         </Flex.Section>
     );
 }
