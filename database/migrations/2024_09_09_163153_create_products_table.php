72f7ec817a293ab8e58f92178852d329ecddd996
MikeeOo <michal.olszewskki@gmail.com>
2025-01-08 09:05:51 +0100

    fixed products migration

--- database/migrations/2024_09_09_163153_create_products_table.php
+++ database/migrations/2024_09_09_163153_create_products_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
     /**
@@ -12,16 +11,19 @@ return new class () extends Migration {
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            // should you do constrained() // because if you delete admin it will delete all the users...
+            // it will be good on multi-user shop but not there
+            // $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('image_url')->nullable();
             $table->string('brand');
             $table->string('category');
-            $table->longText('description');
+            $table->longText('description')->nullable();
             $table->unsignedInteger('stock_quantity')->default(0);
-            $table->decimal('price', 10, 2);
-            $table->decimal('rating', 2, 1)->default(0);
+            $table->decimal('price', 8, 2)->unsigned();
+            $table->decimal('rating', 2, 1)->unsigned()->default(0);
             $table->unsignedInteger('reviews_count')->default(0);
+
             $table->timestamps();
         });
     }
