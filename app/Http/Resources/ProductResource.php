97c3956a8acb4176c7b5fda0e022137a62d5f344
MikeeOo <michal.olszewskki@gmail.com>
2025-01-29 11:01:51 +0100

    Camelized keys in ProductResource

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,9 +2,8 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductResource extends JsonResource
+class ProductResource extends BaseResource
 {
     /**
      * Transform the resource into an array.
@@ -13,25 +12,25 @@ class ProductResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return [
+        return $this->camelizeKeys([
             'type' => 'products',
             'id' => (string)$this->id,
             'attributes' => [
-                'name' => $this->name,
-                'image_url' => $this->image_url,
-                'brand' => $this->brand,
-                'category' => $this->category,
-                'description' => $this->description,
-                'stock_quantity' => $this->stock_quantity,
-                'price' => $this->price,
-                'rating' => $this->rating,
-                'reviews_count' => $this->reviews_count,
-                'created_at' => $this->created_at->toIso8601String(),
-                'updated_at' => $this->updated_at->toIso8601String(),
+            'name' => $this->name,
+            'image_url' => $this->image_url,
+            'brand' => $this->brand,
+            'category' => $this->category,
+            'description' => $this->description,
+            'stock_quantity' => $this->stock_quantity,
+            'price' => $this->price,
+            'rating' => $this->rating,
+            'reviews_count' => $this->reviews_count,
+            'created_at' => $this->created_at->toIso8601String(),
+            'updated_at' => $this->updated_at->toIso8601String(),
             ],
             'links' => [
                 'self' => route('products.show', $this->id),
             ],
-        ];
+        ]);
     }
 }
